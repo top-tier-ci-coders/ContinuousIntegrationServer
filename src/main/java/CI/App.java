4757716c329dead8a86c4170aa44c52269e5687f
Kartal Kaan Bozdogan <bozdogan@kth.se>
2019-01-31 13:23:04 +0100

    Feat #5  Create a dummy branch that builds and tests correctly.
    Also created a small file so that we can test the "pull branch" function

--- src/main/java/CI/App.java
+++ src/main/java/CI/App.java
@@ -4,11 +4,8 @@
 package CI;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Hello world!");
     }
 }
