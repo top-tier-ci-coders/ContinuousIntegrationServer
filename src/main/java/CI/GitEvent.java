4dec976bff5c0c4a03e2a82f27f0be6b4dd811da
Kartal Kaan Bozdogan <bozdogan@kth.se>
2019-01-31 11:28:52 +0100

    feat #22 Add fields to GitEvent

--- src/main/java/CI/GitEvent.java
+++ src/main/java/CI/GitEvent.java
@@ -1,10 +1,13 @@
 package CI;
 
 /** 
- *  @author Marcus Östling
+ *  @author Marcus Östling, Kartal Kaan Bozdoğan
  *  Class that stores information from a web hook request.
  */   
 public class GitEvent
 {
-
-}
+	public EventType eventType;
+	public string pusherName;
+	public string pusherEmail;
+	public string branchName;
+}	
